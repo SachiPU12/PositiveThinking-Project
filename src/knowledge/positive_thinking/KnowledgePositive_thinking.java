28a20228cd4d6e17ced519b854e8c07b2fd6d31b
Sachi Udawaththa <83857134+SachiPU12@users.noreply.github.com>
2021-07-19 20:14:53 +0530

    Add files via upload

---
+++ src/knowledge/positive_thinking/KnowledgePositive_thinking.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package knowledge.positive_thinking;
+
+/**
+ *
+ * @author Internet
+ */
+public class KnowledgePositive_thinking {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new NewJFrame_Main().setVisible(true);
+    }
+    
+}
